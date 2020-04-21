8a5c2a21168b9057a659a206b292bd54344fdbdc
Ian Halstead <iwhalstead10@gmail.com>
2020-04-21 16:47:40 -0600

    Finished logic for tic tac toe

--- Tic-tac-toe/js/main.js
+++ Tic-tac-toe/js/main.js
@@ -19,6 +19,7 @@ for (let i = 0; i < 3; i++) {
 				text.classList.add(textContent);
 				text.innerText = textContent;
 				grid[i][j] = textContent;
+				gameLogic(textContent);
 				isXTurn = !isXTurn;
 			}
 			columnsArray[i][j].appendChild(text);
@@ -26,11 +27,55 @@ for (let i = 0; i < 3; i++) {
 	}
 }
 
-const gameLogic = () => {
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 3; j++) {
-			if (grid[i][j] === textContent) {
+const gameLogic = (textContent) => {
+	let count = 0;
+	if (grid[0][0] === textContent && grid[0][1] === textContent && grid[0][2] === textContent) {
+		console.log('Win');
+	}
+	if (grid[1][0] === textContent && grid[1][1] === textContent && grid[1][2] === textContent) {
+		console.log('Win');
+	}
+	if (grid[2][0] === textContent && grid[2][1] === textContent && grid[2][2] === textContent) {
+		console.log('Win');
+	}
+
+	if (grid[0][0] === textContent && grid[1][0] === textContent && grid[2][0] === textContent) {
+		console.log('Win');
+	}
+	if (grid[0][1] === textContent && grid[1][1] === textContent && grid[2][1] === textContent) {
+		console.log('Win');
+	}
+	if (grid[0][2] === textContent && grid[1][2] === textContent && grid[2][2] === textContent) {
+		console.log('Win');
+	}
+
+	if (grid[0][0] === textContent && grid[1][1] === textContent && grid[2][2] === textContent) {
+		console.log('Win');
+	}
+	if (grid[0][2] === textContent && grid[1][1] === textContent && grid[2][0] === textContent) {
+		console.log('Win');
+	}
+
+	for (let k = 0; k < 3; k++) {
+		for (let l = 0; l < 3; l++) {
+			if (grid[k][l]) {
+				count++;
 			}
 		}
 	}
+
+	if (count === 9) {
+		draw();
+	}
+};
+
+const win = (winner) => {
+	if (winner === x) {
+	}
+	else {
+	}
+};
+
+const draw = () => {
+	location.reload();
 };
